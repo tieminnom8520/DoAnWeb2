6ab98517d2e42ffd653fc88bc136ac4cda483770
Bravo 15 <Bravo 15@MSI>
2023-05-16 13:58:30 +0700

    sửa num_id

--- mvc/models/authModal.php
+++ mvc/models/authModal.php
@@ -1,5 +1,11 @@
 <?php
 require_once "./mvc/core/basehref.php";
+
+//Kiểm tra và khởi tạo giá trị của biến $_SESSION['id_num']
+if (!isset($_SESSION['id_num'])) {
+    $_SESSION['id_num'] = 2;
+}
+// unset($_SESSION['id_num']);
 class authModal extends db{
     private function _query($sql)
     {
@@ -22,24 +28,16 @@ class authModal extends db{
             return [];
         }     
     } 
-    public function generateId(){
-        $typesql = "SELECT * FROM taikhoan";
-        $query1 = $this->_query($typesql);
-        $danh_sach_tai_khoan = [];
-        while ($row = mysqli_fetch_assoc($query1)) {
-            array_push($danh_sach_tai_khoan, $row);
-        }
-        $id_num = count($danh_sach_tai_khoan)+1;
-        $id = "KH".$id_num;
-        return $id;
-    }
+
     public function authRegister($ten,$password, $email,$sdt){
         $password = password_hash($password, PASSWORD_DEFAULT);
         if(!($ten&&$password&& $email&&$sdt)) return "<script>
             alert('Thông tin nhập thiếu. Mời nhập lại');
             location.href = '".geturl()."/login/registerView';
         </script>";
-        $id = $this->generateId();
+        $id = "KH" . $_SESSION['id_num'];
+        $_SESSION['id_num']++;
+
         $typesql1 = "insert into taikhoan (id_taikhoan,ten,	password,	email,	sdt)
         values ('".$id."','".$ten."', '".$password."', '".$email."', '".$sdt."');";
         if (!$this->_query($typesql1)) {

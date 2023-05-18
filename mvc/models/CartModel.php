199182b3a4806ff5e3e80799d8d3bcbf71440045
tieminnom8520 <nghiempham0404@gmail.com>
2023-05-18 21:41:53 +0700

    Sửa biểu mẫu admin sản phẩm, size N

--- mvc/models/CartModel.php
+++ mvc/models/CartModel.php
@@ -1,22 +1,22 @@
 <?php
     class CartModel extends db{
         function getAllOrders($user_id){
-            $qr = "SELECT * FROM order_product WHERE user_taikhoan='".$user_id."'";
+            $qr = "SELECT * FROM ct_don_dat WHERE user_taikhoan='".$user_id."'";
             return mysqli_query($this->connect, $qr);
         }
 
         function payOrder($order_id){
-            $qr = "UPDATE order_product SET order_status=1 WHERE order_id='".$order_id."'";
+            $qr = "UPDATE ct_don_dat SET order_status=1 WHERE order_id='".$order_id."'";
             mysqli_query($this->connect, $qr);
         }
 
         function removeOrder($order_id, $order_status){
             if ($order_status == 0){
-                $qr = "DELETE FROM order_product WHERE order_id='".$order_id."'";
+                $qr = "DELETE FROM ct_don_dat WHERE order_id='".$order_id."'";
                 mysqli_query($this->connect, $qr);
             }
             else {
-                $qr = "UPDATE order_product SET order_in_cart=0 WHERE order_id='".$order_id."'";
+                $qr = "UPDATE ct_don_dat SET order_in_cart=0 WHERE order_id='".$order_id."'";
                 mysqli_query($this->connect, $qr);
             }
         }
--- mvc/models/paymentModel.php
+++ mvc/models/paymentModel.php
@@ -2,7 +2,7 @@
     class paymentModel extends db{
         
         public function add($quantity, $user_taikhoan, $id_mon){
-            $qr = "INSERT INTO order_product (quantity, user_taikhoan, id_mon) VALUES (".$quantity.", ".$user_taikhoan.", ".$id_mon.")";
+            $qr = "INSERT INTO ct_don_dat (quantity, user_taikhoan, id_mon) VALUES (".$quantity.", ".$user_taikhoan.", ".$id_mon.")";
             mysqli_query($this->connect, $qr);
         }
     }
